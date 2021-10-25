5e714315350586a9c32b817bd0daacc2139685ff
Roy Miana <roymiana15@gmail.com>
2021-10-25 11:15:57 +0800

    a lot of changes lmao

--- services/ChannelService.js
+++ services/ChannelService.js
@@ -9,6 +9,7 @@ const ChannelService = {
   addChannel: ({ serverId, body }) =>
     axios.post(`${BASE_URL}/servers/${serverId}/channels`, body),
   deleteChannel: ({ id }) => axios.delete(`${BASE_URL}/channels/${id}`),
+  updateChannel: ({ id, body }) => axios.patch(`${BASE_URL}/channels/${id}`, body),
 };
 
 export default ChannelService;
--- utils/message.js
+++ utils/message.js
@@ -1,4 +1,6 @@
 import ChannelService from '../services/ChannelService.js';
+import ServerService from '../services/ServerService.js';
+import { MessageEmbed } from 'discord.js';
 
 const messageAll = async (guild, content) => {
   let serverId = guild.id;
@@ -11,23 +13,79 @@ const messageAll = async (guild, content) => {
   return 'sent';
 };
 
-const embedQuestion = async (MessageEmbed, content) => {
+const embedQuestion = async (content) => {
   const exampleEmbed = new MessageEmbed()
     .setColor('#0099ff')
-    .setTitle('Question ' + questions.length)
-    .setDescription(content);
+    .setTitle('Question: ' + content)
 
-  return exampleEmbed;
+
+  return {embeds: [exampleEmbed]};
 };
 
-const embedAnswers = async (MessageEmbed, content) => {
+const embedAnswers = async (content) => {
   const embedans = new MessageEmbed()
     .setColor('#ffda4f')
-    .setTitle(
-      'Question ' + questions.length + ': ' + questions[questions.length - 1]
-    )
     .setDescription(content);
-  return embedans;
+  return {embeds: [embedans]};
 };
 
-export { messageAll, embedQuestion, embedAnswers };
+const getAnswer = async (serverId) => {
+    let { data } = await ChannelService.getChannels({ serverId: serverId });
+    let answers = "ANSWERS: \n";
+
+    data.map(curr => {
+        if (curr.id != 'gen' + serverId) {
+            answers += curr.name +": "+ curr.currentAnswer +"\n";
+        }
+    });
+
+    return answers;
+}
+
+const clearAnswer = async (serverId) => {
+    let { data } = await ChannelService.getChannels({ serverId: serverId });
+
+    data.map(curr => {
+        if (curr.id != 'gen' + serverId) {
+            ChannelService.updateChannel({
+                id: 'team' + curr.id,
+                body: {
+                    currentAnswer: '',
+                }
+            });
+        }
+    });
+
+    return answers;
+}
+
+const setTimer = async (guild) => {
+    let serverId = guild.id;
+    let { data } = await ServerService.getServer({ id: serverId });
+    let time = 60;
+    messageAll(guild, time + ' seconds left');
+    var interval = setInterval(async () => {
+        time -= 10;
+        messageAll(guild, time + ' seconds left');
+        if(time <= 0){
+            messageAll(guild, 'TIMES UP!');
+            ServerService.updateServer({
+                id: serverId,
+                body: {
+                is_timeup: true,
+                },
+            });
+
+            let answers = await getAnswer(serverId);
+
+            let embedAns = await embedAnswers(answers);
+            ChannelService.getChannel({ channelId: 'gen'+serverId }).then(res => {
+                guild.channels.cache.get(res.data.answers_channel).send(embedAns);
+            });
+            
+            clearInterval(interval);
+        }
+    }, 10000)
+}
+
+export { messageAll, embedQuestion, embedAnswers, clearAnswer, setTimer};
